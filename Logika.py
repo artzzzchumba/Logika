11a2018a3e1bbd53d1549e3378f8be3ed3efb6bc
artem <artem@LAPTOP-003DFP6U>
2022-10-16 14:48:39 +0300

    Добавьте файлы проекта.

---
+++ Logika.py
@@ -0,0 +1,13 @@
+a=True
+b=False
+print('Логическое AND')
+print('a and a=',a and a)
+print('a and b=',a and b)
+print('b and b=',b and b)
+print('\nЛогическое OR')
+print('a or a=',a or a)
+print('a or b=',a or b)
+print('b or b',b or b)
+print('\nлогическое NOT')
+print('a=',a,'\tnot a=',not a)
+print('b=',b,'\tnot b=',not b)
